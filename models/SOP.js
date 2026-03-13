b43265b416b814b623530ed4d0309a8853486ebe
deepakjatov <deepak.jatov@arizonsystems.com>
2026-03-14 03:14:29 +0530

    updated for pdf

--- models/SOP.js
+++ models/SOP.js
@@ -11,7 +11,9 @@ const sopSchema = new mongoose.Schema({
     requiredRoles: [{ type: String }],
     references: [{ type: String }], // NEW: Tracks related SOP IDs
     ownerSystem: { type: String, default: 'SOP_Intelligence' },
-    pdfPath: { type: String } 
+    pdfPath: { type: String }, 
+    pdfPathBase64: { type: String },
+    data: { type: Object }, 
 }, { timestamps: true });
 
 module.exports = mongoose.model('SOP', sopSchema);
--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const auditRoutes = require('./routes/auditRoutes');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 // Connect Database
 connectDB();
